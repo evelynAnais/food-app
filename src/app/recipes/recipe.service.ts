6e2cf06ae6a5d19e9c2d05f0eba0064b3a06bdbb
evelynAnais <evelyn.a.n.d.30@gmail.com>
2022-12-10 16:04:45 -0700

    using service for component cross communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('Test', 'Description', 'https://www.feastingathome.com/wp-content/uploads/2021/01/How-to-grow-sprouts-2-1.jpg'),
     new Recipe('Test 2', 'Description 2', 'https://www.feastingathome.com/wp-content/uploads/2021/01/How-to-grow-sprouts-2-1.jpg')
