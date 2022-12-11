be14eba643b459b30c2c11fc47ef01a8dbfb6f46
evelynAnais <evelyn.a.n.d.30@gmail.com>
2022-12-10 17:33:29 -0700

    adding ingredients to list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,41 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredients.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
-
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test', 'Description', 'https://www.feastingathome.com/wp-content/uploads/2021/01/How-to-grow-sprouts-2-1.jpg'),
-    new Recipe('Test 2', 'Description 2', 'https://www.feastingathome.com/wp-content/uploads/2021/01/How-to-grow-sprouts-2-1.jpg')
+    new Recipe(
+      'Enchiladas',
+      'Traditional Mexican dish',
+      'https://thedirtygyro.com/wp-content/uploads/2022/02/Beef-Enchilladas1055023.jpg',
+      [
+        new Ingredient('Tortillas', 10),
+        new Ingredient('Cheese Bag', 1),
+      ]),
+    new Recipe(
+      'Tacos',
+      'Traditional Mexican dish',
+      'https://i2.wp.com/pressurecookingtoday.com/wp-content/uploads/2022/01/Pork_and_Hominy_Stew_Posole_Instant_Pot_Recipe_Pressure_Cooking_Today_PCT-2309.jpg',
+      [
+        new Ingredient('Hominy', 1),
+        new Ingredient('Corn', 1),
+        new Ingredient('Onion', 1),
+      ])
   ];
+
+  constructor(
+    private shoppingListService: ShoppingListService
+  ) { }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
