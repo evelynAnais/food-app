43af8c3348efc37ae142a7a5f6441e5541271cf0
evelynAnais <evelyn.a.n.d.30@gmail.com>
2023-03-18 12:35:11 -0600

    added submit handler

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -20,6 +20,9 @@ export class RecipesListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+      this.recipes = recipes;
+    });
     this.recipes = this.recipeService.getRecipes();
   }
 
